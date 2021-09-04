c03b1a4099b4bedce9d987cb4d4e518cf6f5a8dd
Papekhaly <papekhaly96@outlook.fr>
2021-09-04 21:45:28 +0200

    test infra

--- tests/test_elasticsearch.py
+++ tests/test_elasticsearch.py
@@ -1,11 +1,5 @@
-def test_nginx_is_installed(host):
-    elastic = host.package("{{ elasticsearch_hostname }}")
-    assert elastic.is_installed
-    assert elastic.version.startswith("7.4.0")
-
-
 def test_elastic_running_and_enabled(host):
-    elastic = host.service("{{ elasticsearch_hostname }}")
+    elastic = host.docker("elasticsearch01")
     assert elastic.is_running
-    assert elastic.is_enabled
+   
 
