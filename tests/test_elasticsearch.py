796a5d7a05adfdd3dd650dad3963b04dfa7ffa63
Papekhaly <papekhaly96@outlook.fr>
2021-09-05 00:31:11 +0200

    tests integration

--- tests/test_elasticsearch.py
+++ tests/test_elasticsearch.py
@@ -1,5 +1,9 @@
+import pytest, webtest
+
+
 def test_elastic_running_and_enabled(host):
     elastic = host.docker("elasticsearch01")
     assert elastic.is_running
-   
 
+def test_elastic_is_listening(host):
+    assert host.socket('tcp://127.0.0.1:9200').is_listening
